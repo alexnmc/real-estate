b998c6e5ed59cfc0d643fb43f0688c1381c29afa
Alexander <nemechekalexander@gmail.com>
2019-01-15 20:06:13 -0500

    new

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = props => {
+    return (
+        <div className="home">
+            <div className = "homeText">
+            <p className = "intro2">Valentina Pan</p>
+            <p className = "intro">FLORIDA REAL ESTATE PROFESSIONAL</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <Link className = "link" to="/">Home</Link>
+            <Link className = "link" to="/about">About</Link>
+            <Link className = "link" to="/contact">Contact</Link>
+        </div>
+    )
+}
+
+export default Navbar

fcebe9f0a830e3f3c1ce96d3826d27df721dba81
Alexander <nemechekalexander@gmail.com>
2019-01-16 17:24:32 -0500

    new

--- src/About.js
+++ src/About.js
@@ -3,7 +3,14 @@ import React from 'react'
 const About = props => {
     return (
         <div className="about">
-            
+        <div className="about2">
+        <a href = "https://www.facebook.com/valentina.pan.9">
+        <div className="facebook"></div>
+        </a>
+        <a href = "https://www.linkedin.com/in/valentina-pan-50a3a7139/">
+        <div className=" linkedin"></div>
+        </a>
+        </div> 
         </div>
     )
 }

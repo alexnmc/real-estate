c2ea0bdd1179652b98f86cdcdea2db03f81d81ac
Alexander <nemechekalexander@gmail.com>
2019-01-16 17:53:11 -0500

    update

--- src/About.js
+++ src/About.js
@@ -4,12 +4,18 @@ const About = props => {
     return (
         <div className="about">
         <div className="about2">
-        <a href = "https://www.facebook.com/valentina.pan.9">
+       
+        <div className = "photo"></div>   
+        
+        <a href = "https://www.facebook.com/Valentina-Pan-Real-Estate-Agent- 112054539337268/?ti=as">
         <div className="facebook"></div>
         </a>
+       
         <a href = "https://www.linkedin.com/in/valentina-pan-50a3a7139/">
         <div className=" linkedin"></div>
         </a>
+        
+       
         </div> 
         </div>
     )

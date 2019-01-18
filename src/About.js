4641a10c32e220d5ae70ca233af80fb4d39db88d
Alexander <nemechekalexander@gmail.com>
2019-01-18 16:53:36 -0500

    new

--- src/About.js
+++ src/About.js
@@ -2,26 +2,39 @@ import React from 'react'
 
 const About = props => {
     return (
-        <div className="about" id = '2'>
+        <div className="about" id = '2'  >
         
-        <div className="about2">
+                <div  id="slideshow">
+                    
+                    <div class="elemnt"><span>Rentals</span></div>
+                    <div class="elemnt1"><span>Exclusive Listings</span></div>
+                    <div class="elemnt2"><span>Luxury Living</span></div>
+                    <div class="elemnt3"><span>Sales</span></div>
+                
+            
+                
+                <div className="about2">
+                
+                    <div className = "photo"></div>   
+                    
+                    <div className = "logos">
+                    
+                            <a href = "https://www.facebook.com/Valentina-Pan-Real-Estate-Agent- 112054539337268/?ti=as">
+                            <div className="facebook"></div>
+                            </a>
+                        
+                            <a href = "https://www.linkedin.com/in/valentina-pan-50a3a7139/">
+                            <div className=" linkedin"></div>
+                            </a>
+                    </div>
+                
        
-        <div className = "photo"></div>   
+                </div> 
         
-        <div className = "logos">
-        
-        <a href = "https://www.facebook.com/Valentina-Pan-Real-Estate-Agent- 112054539337268/?ti=as">
-        <div className="facebook"></div>
-        </a>
-       
-        <a href = "https://www.linkedin.com/in/valentina-pan-50a3a7139/">
-        <div className=" linkedin"></div>
-        </a>
+               </div>
         </div>
         
-       
-        </div> 
-        </div>
+        
     )
 }
 
